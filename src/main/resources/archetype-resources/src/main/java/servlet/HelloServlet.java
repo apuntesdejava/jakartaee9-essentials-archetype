37c0bf3746629d8c0e641a38e29b5630efa41cb4
Diego Silva <diego.silva@vasslatam.com>
2022-06-01 12:55:54 -0500

    Archivos faltantes
    Faltaban archivos y sobraban otros. La versión actual es la 0.1

--- src/main/resources/archetype-resources/src/main/java/servlet/HelloServlet.java
+++ src/main/resources/archetype-resources/src/main/java/servlet/HelloServlet.java
@@ -28,13 +28,12 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Hello World from Servlet!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
-        // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
