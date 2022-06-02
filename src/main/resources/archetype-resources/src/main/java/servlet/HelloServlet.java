cc8f762fb89c81a48608ce6c943396c39414937c
Diego Silva <diego.silva@vasslatam.com>
2022-06-01 21:33:01 -0500

    Import mejorados

--- src/main/resources/archetype-resources/src/main/java/servlet/HelloServlet.java
+++ src/main/resources/archetype-resources/src/main/java/servlet/HelloServlet.java
@@ -18,19 +18,25 @@
  */
 package ${package}.servlet;
 
-import java.io.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
 
+    @Override
     public void init() {
         message = "Hello World from Servlet!";
     }
 
+    @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
@@ -40,6 +46,7 @@ public class HelloServlet extends HttpServlet {
         out.println("</body></html>");
     }
 
+    @Override
     public void destroy() {
     }
 }
